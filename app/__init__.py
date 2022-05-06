df874f2c6a155c3462459da4842a739d8bd583ab
Yinghao Zhu <yhzhu99@gmail.com>
2022-05-06 17:08:34 +0800

    chore: update imported packages in different files

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,22 @@
 from omegaconf import OmegaConf
+from sklearn.model_selection import KFold, StratifiedKFold
+import torch
+from torch import nn
+import torch.nn.utils.rnn as rnn_utils
+from torch.utils import data
+from torch.autograd import Variable
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader,TensorDataset,random_split,SubsetRandomSampler, ConcatDataset, Subset
 
 import app.models as models
 
 def create_app(my_pipeline):
     # Load config
-    default_pipeline = OmegaConf.load('configs/gru_tongji_epoch50_fold10_bs64.yaml')
+    my_pipeline = OmegaConf.load('configs/gru_tongji_epoch50_fold10_bs64.yaml')
     # Load dataset
-    dataset = OmegaConf.load(f'configs/_base_/dataset/{default_pipeline.dataset}.yaml')
+    dataset = OmegaConf.load(f'configs/_base_/dataset/{my_pipeline.dataset}.yaml')
     # Merge config
-    cfg = OmegaConf.merge(dataset, default_pipeline, my_pipeline)
+    cfg = OmegaConf.merge(dataset, my_pipeline)
     # Create model
     model = eval(f'models.{cfg.model}(input_lab_dim={cfg.labtest_dim}, input_demo_dim={cfg.demographic_dim}, hidden_dim={32}, output_dim={1})')
     # Print model
--- main.py
+++ main.py
@@ -1,23 +1,4 @@
-import numpy as np
-import argparse
-import os
-import re
-import pickle
-import datetime
-import random
-import math
-import copy
-
 from omegaconf import OmegaConf
-
-from sklearn.model_selection import KFold, StratifiedKFold
-import torch
-from torch import nn
-import torch.nn.utils.rnn as rnn_utils
-from torch.utils import data
-from torch.autograd import Variable
-import torch.nn.functional as F
-from torch.utils.data import Dataset, DataLoader,TensorDataset,random_split,SubsetRandomSampler, ConcatDataset, Subset
 
 import app.models as models
 from app import create_app
