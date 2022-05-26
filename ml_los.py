86832d090add0b76e6592c045c710f78a92bf14f
Yinghao Zhu <yhzhu99@gmail.com>
2022-05-26 14:37:55 +0800

    chore: data_path join str

--- ml_los.py
+++ ml_los.py
@@ -89,15 +89,10 @@ def test(x, y, model):
 
 
 if __name__ == "__main__":
-    data_path = "./dataset/tongji/processed_data/"
-
-    x = pickle.load(open("./dataset/tongji/processed_data/x.pkl", "rb"))
-
-    y = pickle.load(open("./dataset/tongji/processed_data/y.pkl", "rb"))
-
-    x_lab_length = pickle.load(
-        open("./dataset/tongji/processed_data/visits_length.pkl", "rb")
-    )
+    data_path = "./dataset/hm/processed_data/"
+    x = pickle.load(open(data_path + "_x.pkl", "rb"))
+    y = pickle.load(open(data_path + "_y.pkl", "rb"))
+    x_lab_length = pickle.load(open(data_path + "visits_length.pkl", "rb"))
 
     x = x.numpy()
     y = y.numpy()
