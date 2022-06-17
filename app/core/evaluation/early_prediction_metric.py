3ed817149cba28d7e4c67e899e94a26df3b8a5e8
Yinghao Zhu <yhzhu99@gmail.com>
2022-06-18 00:01:52 +0800

    chore: update type annotations for our proposed metrics

--- app/core/evaluation/early_prediction_metric.py
+++ app/core/evaluation/early_prediction_metric.py
@@ -1,35 +1,42 @@
 def early_prediction_outcome_metric(y_true, predictions, verbose=1):
     """
     > y_true: np.ndarray
+      shape (num_patients, 2), 2 --> [outcome, los]
+      eg: 2 records, they have outcome = 1, 0 separately
+          then y_true = [[1, 5], [0, 3]]
+          5 and 3 denotes their length of stay
     > predictions: np.ndarray
     return metric (type: List)
+
+    note:
+      - y/predictions are already flattened here
+      - so we don't need to consider visits_length
     """
     metric = []
-    num_patient, max_visits_len, _ = y_true.shape
-    for i in range(num_patient):
-        for j in range(max_visits_len):
-            cur_out = predictions[i, j, :]
-            cur_gt = y_true[i, j, :]
-            if cur_out[0] > 0.5 and cur_gt[0] == 1:  # 预测死亡, 实际最终死亡
-                # 判断实际的当前los
-                if cur_gt[1] >= 2:  # 早期预测对
-                    metric.append(1)
-                elif 2 > cur_gt[1] > 1:  # 中期预测对
-                    score = cur_gt[1] - 1
-                    metric.append(score)
-                elif cur_gt[1] <= 1:  # 晚期预测对
-                    metric.append(0)
-            elif cur_out[0] <= 0.5 and cur_gt[0] == 1:  # 预测存活, 实际最终死亡
-                if cur_gt[1] >= 2:  # 早期预测错，没关系
-                    metric.append(0)
-                elif 2 > cur_gt[1] > 1:  # 中期预测错
-                    score = cur_gt[1] - 2
-                    metric.append(score)
-                elif cur_gt[1] <= 1:  # 晚期预测错
-                    metric.append(-1)
-            else:
-                # 其他case不关心，score为0即可
+    num_records = len(y_true)
+    for i in range(num_records):
+        cur_out = predictions[i, :]
+        cur_gt = y_true[i, :]
+        if cur_out[0] > 0.5 and cur_gt[0] == 1:  # 预测死亡, 实际最终死亡
+            # 判断实际的当前los
+            if cur_gt[1] >= 2:  # 早期预测对
+                metric.append(1)
+            elif 2 > cur_gt[1] > 1:  # 中期预测对
+                score = cur_gt[1] - 1
+                metric.append(score)
+            elif cur_gt[1] <= 1:  # 晚期预测对
                 metric.append(0)
+        elif cur_out[0] <= 0.5 and cur_gt[0] == 1:  # 预测存活, 实际最终死亡
+            if cur_gt[1] >= 2:  # 早期预测错，没关系
+                metric.append(0)
+            elif 2 > cur_gt[1] > 1:  # 中期预测错
+                score = cur_gt[1] - 2
+                metric.append(score)
+            elif cur_gt[1] <= 1:  # 晚期预测错
+                metric.append(-1)
+        else:
+            # 其他case不关心，score为0即可
+            metric.append(0)
     if verbose:
         print(metric)
     return metric
@@ -38,7 +45,15 @@ def early_prediction_outcome_metric(y_true, predictions, verbose=1):
 def early_prediction_los_metric(y_true, predictions, verbose=1):
     """
     > y_true: np.ndarray
+      shape (num_patients, 2), 2 --> [outcome, los]
+      eg: 2 records, they have outcome = 1, 0 separately
+          then y_true = [[1, 5], [0, 3]]
+          5 and 3 denotes their length of stay
     > predictions: np.ndarray
     return metric (type: List)
+
+    note:
+      - y/predictions are already flattened here
+      - so we don't need to consider visits_length
     """
     pass
