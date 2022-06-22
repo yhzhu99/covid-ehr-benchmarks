31c9248d366bb064587caab1a34d21c2a11bf945
Yinghao Zhu <yhzhu99@gmail.com>
2022-06-22 16:37:55 +0800

    chore(ml): only early prediction task y has 2 val

--- app/datasets/ml.py
+++ app/datasets/ml.py
@@ -8,11 +8,10 @@ def flatten_dataset(x, y, indices, visits_length, case="los"):
     for i in indices:
         for v in range(visits_length[i]):
             x_flat.append(x[i][v])
-            # if case == "los":
-            #     y_flat.append(y[i][v][1])
-            # elif case == "outcome":
-            #     y_flat.append(y[i][v][0])
-            y_flat.append(y[i][v].tolist())
+            if case == "los":
+                y_flat.append(y[i][v][1])
+            elif case == "outcome":
+                y_flat.append(y[i][v].tolist())
     return np.array(x_flat), np.array(y_flat)
 
 
