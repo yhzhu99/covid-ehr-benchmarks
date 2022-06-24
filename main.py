903bf38010bbd87138162ff45e00ba52e6459f8c
Yinghao Zhu <yhzhu99@gmail.com>
2022-06-24 10:53:18 +0800

    chore: delete cfg_options in main.py

--- main.py
+++ main.py
@@ -6,18 +6,7 @@ from app import create_app
 
 if __name__ == "__main__":
     print("===[Start]===")
-    configs_options = [
-        "ml_tongji_outcome_kf10.yaml",
-        "ml_tongji_los_kf10.yaml",
-        "ml_hm_los_kf10.yaml",
-        "gru_tongji_outcome_ep100_kf10_bs64.yaml",
-        "gru_tongji_los_ep100_kf10_bs64.yaml",
-        "gru_tongji_multitask_ep100_kf10_bs64.yaml",
-        "transformer_hm_multitask_ep100_kf10_bs64.yaml",
-        "gru_hm_multitask_ep100_kf10_bs64.yaml",
-        "gru_hm_los_ep100_kf10_bs64.yaml",
-    ]
-    my_pipeline = OmegaConf.load(f"configs/{configs_options[0]}")
+    my_pipeline = OmegaConf.load("configs/tj_gru_multitask_ep100_kf10_bs64_hid64.yaml")
     device = torch.device("cuda:0" if torch.cuda.is_available() == True else "cpu")
     cfg = create_app(my_pipeline, device)
     print("===[End]===")
