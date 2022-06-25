ce1f225e785f536b29b088cf35fc7aeae9ec9d0e
Yinghao Zhu <yhzhu99@gmail.com>
2022-06-25 15:09:05 +0800

    chore: modify adacare model to fit framework

--- app/models/backbones/adacare.py
+++ app/models/backbones/adacare.py
@@ -34,7 +34,7 @@ class Sparsemax(nn.Module):
 
         self.dim = -1 if dim is None else dim
 
-    def forward(self, input, device="cuda"):
+    def forward(self, input):
         original_size = input.size()
         input = input.view(-1, input.size(self.dim))
 
@@ -45,7 +45,7 @@ class Sparsemax(nn.Module):
 
         zs = torch.sort(input=input, dim=dim, descending=True)[0]
         range = torch.arange(
-            start=1, end=number_of_logits + 1, device=device, dtype=torch.float32
+            start=1, end=number_of_logits + 1, dtype=torch.float32
         ).view(1, -1)
         range = range.expand_as(zs)
 
@@ -122,7 +122,7 @@ class Recalibration(nn.Module):
         self.nn_rescale = nn.Linear(scale_dim, channel)
         self.sparsemax = Sparsemax(dim=1)
 
-    def forward(self, x, device="cuda"):
+    def forward(self, x):
         b, c, t = x.size()
 
         y_origin = x[:, :, -1]
@@ -134,7 +134,7 @@ class Recalibration(nn.Module):
         if self.activation == "sigmoid":
             y = torch.sigmoid(y)
         else:
-            y = self.sparsemax(y, device)
+            y = self.sparsemax(y)
         return x * y.expand_as(x), y
 
 
@@ -145,19 +145,18 @@ class AdaCare(nn.Module):
         kernel_size=2,
         kernel_num=64,
         input_dim=76,
-        output_dim=1,
+        # output_dim=1,
         dropout=0.5,
         r_v=4,
         r_c=4,
         activation="sigmoid",
-        device="cuda",
     ):
         super(AdaCare, self).__init__()
         self.hidden_dim = hidden_dim
         self.kernel_size = kernel_size
         self.kernel_num = kernel_num
         self.input_dim = input_dim
-        self.output_dim = output_dim
+        # self.output_dim = output_dim
         self.dropout = dropout
 
         self.nn_conv1 = CausalConv1d(input_dim, kernel_num, kernel_size, 1, 1)
@@ -174,20 +173,20 @@ class AdaCare(nn.Module):
             input_dim, r_v, use_h=False, use_c=True, activation=activation
         )
         self.rnn = nn.GRUCell(input_dim + 3 * kernel_num, hidden_dim)
-        self.nn_output = nn.Linear(hidden_dim, output_dim)
+        # self.nn_output = nn.Linear(hidden_dim, output_dim)
         self.nn_dropout = nn.Dropout(dropout)
 
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
         self.tanh = nn.Tanh()
 
-    def forward(self, input, device):
+    def forward(self, input):
         # input shape [batch_size, timestep, feature_dim]
         batch_size = input.size(0)
         time_step = input.size(1)
         feature_dim = input.size(2)
 
-        cur_h = Variable(torch.zeros(batch_size, self.hidden_dim)).to(device)
+        cur_h = Variable(torch.zeros(batch_size, self.hidden_dim))
         inputse_att = []
         convse_att = []
         h = []
@@ -201,11 +200,9 @@ class AdaCare(nn.Module):
         conv_res = self.relu(conv_res)
 
         for cur_time in range(time_step):
-            convse_res, cur_convatt = self.nn_convse(
-                conv_res[:, :, : cur_time + 1], device=device
-            )
+            convse_res, cur_convatt = self.nn_convse(conv_res[:, :, : cur_time + 1])
             inputse_res, cur_inputatt = self.nn_inputse(
-                input[:, : cur_time + 1, :].permute(0, 2, 1), device=device
+                input[:, : cur_time + 1, :].permute(0, 2, 1)
             )
             cur_input = torch.cat((convse_res[:, :, -1], inputse_res[:, :, -1]), dim=-1)
 
@@ -215,10 +212,12 @@ class AdaCare(nn.Module):
             inputse_att.append(cur_inputatt)
 
         h = torch.stack(h).permute(1, 0, 2)
-        h_reshape = h.contiguous().view(batch_size * time_step, self.hidden_dim)
+        output = h.contiguous().view(batch_size, time_step, self.hidden_dim)
         if self.dropout > 0.0:
-            h_reshape = self.nn_dropout(h_reshape)
-        output = self.nn_output(h_reshape)
-        output = self.sigmoid(output)
-        output = output.contiguous().view(batch_size, time_step, self.output_dim)
-        return output, inputse_att
+            output = self.nn_dropout(output)
+        # output = self.nn_output(h_reshape)
+        # output = self.sigmoid(output)
+        # output = output.contiguous().view(batch_size, time_step, self.output_dim)
+        # output = h_reshape.contiguous().view(batch_size, time_step, self.hidden_dim)
+        # return output, inputse_att
+        return output
