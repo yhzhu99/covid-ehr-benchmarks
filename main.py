dbe1c1c16fc5a86a576b587452ae5011d8970d63
19231133 <19231133@buaa.edu.cn>
2022-07-26 23:44:08 +0800

    set timeout for ml model

--- main.py
+++ main.py
@@ -4,9 +4,53 @@ import pathlib
 import torch
 from omegaconf import OmegaConf
 
+import signal
+import time
+import threading
+
 from app import create_app
 
-if __name__ == "__main__":
+class MyThread(threading.Thread):
+    def __init__(self, target, args=()):
+        super(MyThread, self).__init__()
+        self.func = target
+        self.args = args
+
+    def run(self):
+        # 接受返回值
+        self.result = self.func(*self.args)
+
+    def get_result(self):
+        # 线程不结束,返回值为None
+        try:
+            return self.result
+        except Exception:
+            return None
+
+def limit_decor(timeout, granularity):
+    """
+        timeout 最大允许执行时长, 单位:秒
+        granularity 轮询间隔，间隔越短结果越精确同时cpu负载越高
+        return 未超时返回被装饰函数返回值,超时则返回 None
+    """
+    def functions(func):
+        def run(*args):
+            thre_func = MyThread(target=func, args=args)
+            thre_func.setDaemon(True)
+            thre_func.start()
+            sleep_num = int(timeout//granularity)
+            for i in range(0, sleep_num):
+                infor = thre_func.get_result()
+                if infor:
+                    return infor
+                else:
+                    time.sleep(granularity)
+            return None
+        return run
+    return functions
+
+@limit_decor(600, 10)
+def main():
     pathlib.Path("./checkpoints").mkdir(parents=True, exist_ok=True)
     print("===[Start]===")
     parser = argparse.ArgumentParser("Covid-EMR training script", add_help=False)
@@ -40,3 +84,6 @@ if __name__ == "__main__":
 
     create_app(conf, device)
     print("===[End]===")
+
+if __name__ == "__main__":
+    main()
